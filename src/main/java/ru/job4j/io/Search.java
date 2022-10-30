8f9162ae5fe8bc9b1b9b3b591d8d822b9900d19f
Samsung <tat-milakina@yandex.ru>
2022-10-30 21:01:57 +0300

    5. Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,21 +1,38 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Set;
 import java.util.function.Predicate;
 
 public class Search {
+    static Set<String> extension = Set.of(".txt", ".properties", ".csv");
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".properties")).forEach(System.out::println);
+        validate(args);
+        Path start = Paths.get(args[0]);
+        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    public static void validate(String[] n)  {
+        if (n.length != 2) {
+            throw new IllegalArgumentException("The number of parameters is not enough");
+        }
+        File dir = new File(n[0]);
+        if (!dir.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", dir.getAbsoluteFile()));
+        }
+        if (!extension.contains(n[1])) {
+            throw new IllegalArgumentException(String.format("No such expansion %s", n[1]));
+        }
     }
 }
