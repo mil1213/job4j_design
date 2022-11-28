d6badcf4b7c95edc767419f174d555e047a3c27c
Samsung <tat-milakina@yandex.ru>
2022-11-28 22:15:14 +0300

    3. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -2,7 +2,6 @@ package ru.job4j.io;
 
 import java.io.*;
 import java.net.*;
-import java.sql.SQLOutput;
 
 public class EchoServer {
     public static void main(String[] args) throws IOException {
@@ -12,12 +11,17 @@ public class EchoServer {
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
-                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    String str = in.readLine();
-                    System.out.println(str);
-                    if (str.contains("?msg=Bye")) {
-                        server.close();
-                    }
+                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                     String str = in.readLine();
+                     System.out.println(str);
+                     if (str.contains("?msg=Bye")) {
+                         out.write("Good bye, dear friend.".getBytes());
+                         server.close();
+                     } else if (str.contains("?msg=Hello")) {
+                         out.write("Hello, dear friend.".getBytes());
+                     } else {
+                         out.write("What.".getBytes());
+                     }
                     out.flush();
                 }
             }
